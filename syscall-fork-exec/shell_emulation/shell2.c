710d95cbd108c1526098aaa4a5517ecaa009e26a
giob22 <giovanni.fragola.nl@gmail.com>
2025-10-12 23:44:18 +0200

    Update, I'm looking to add a pipe mechanism

---
+++ syscall-fork-exec/shell_emulation/shell2.c
@@ -0,0 +1,109 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <ctype.h> // for isspace();
+
+int is_background(char * s){
+    int i = strlen(s);
+    i--;
+    while (i >= 0 && (char)s[i] == ' '){
+        i--;
+    }
+    return (((char)s[i] == '&') ? i : -1);
+}
+
+
+int main(){
+
+    pid_t pid;
+    int st;
+    const int NMAX = 50;
+    const int CMAX = 100;
+    const int PMAX = 10;
+    char* argv[NMAX];
+    char* p_argv[PMAX];
+    char comLine[CMAX + 1];
+    char * token; // temp
+    char endShell[5] = "quit";
+
+    int n;
+
+    printf("--SHELL OPENED--\n");
+    
+
+    while(1){
+        printf("my_shell>> ");
+
+        // read
+
+        if(fgets(comLine, CMAX + 1, stdin) == NULL){
+            fprintf(stderr, "errore in lettura");
+            return -1;
+        }
+
+        // put \0 at the end of comLine
+        char* p;
+        if((p = strrchr(comLine, '\n')) != NULL)
+            *p = '\0';
+
+        if (strcmp(comLine, endShell) == 0){
+            printf("--SHELL CLOSED--\n");
+            return 0;
+        }
+        
+        // press enter
+        if(strlen(comLine) == 0){
+            continue;
+        }
+        
+        
+        pid = fork();
+        if(pid < 0){
+            fprintf(stderr,"error during the creation of the process\n");
+            return -1;
+        }else if(pid == 0){
+            //code for the new process
+            
+            // slip the comLine
+            
+            //if & (background)
+            n = 0;
+            token = strtok(comLine, "|");
+            while (token != NULL){
+                p_argv[n] = token;
+                n++;
+                token = strtok(NULL, "|");
+            }
+            int p_n = n;
+            int pos = is_background(p_argv[p_n - 1]);
+            if(pos >= 0){
+                p_argv[p_n - 1][pos] = ' ';
+                pid_t pid_other = fork();
+				if(pid_other < 0){
+					fprintf(stderr, "generazione del processo in backgound fallita.\n");
+					_exit(1);
+                }else if (pid_other > 0){
+                    // I terminated it in order to do a backgound execution
+                    _exit(0);
+                }else if (pid_other == 0){
+                    int fd = open("/dev/null", O_RDONLY);
+
+                    dup2(fd,0) // redirect stdin to /dev/null
+                    close(fd);
+                }
+            }
+            //devi fare solo la parte del pipe
+        
+                        
+                        
+    
+        
+    }
+
+
+    return 0;
+}
