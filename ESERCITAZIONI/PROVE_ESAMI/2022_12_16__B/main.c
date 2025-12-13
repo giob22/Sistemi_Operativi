74a68f89b9f70b29ba39cdb8d498fdb65bb22194
giob22 <98953109+giob22@users.noreply.github.com>
2025-12-13 15:51:14 -0600

    fine prova 2022

--- ESERCITAZIONI/PROVE_ESAMI/2022_12_16__B/main.c
+++ ESERCITAZIONI/PROVE_ESAMI/2022_12_16__B/main.c
@@ -14,20 +14,25 @@
 int main() {
 
 	int id_queue, id_shm;
-	key_t chiave_shm = // TODO
-	key_t chiave_mbx = // TODO
+	key_t chiave_shm = IPC_PRIVATE;// TODO
+	key_t chiave_mbx = IPC_PRIVATE;// TODO
 
-	id_shm = //TODO
-	id_queue = // TODO
+	id_shm = shmget(chiave_shm, sizeof(MonitorRisultati), IPC_CREAT | 0664);//TODO
+	id_queue = msgget(chiave_mbx, IPC_CREAT | 0664);// TODO
 
 
-	MonitorRisultati * ls =// TODO
+	MonitorRisultati * ls = shmat(id_shm, NULL, 0);
 	if(ls==(void*)-1){
 		perror("Errore SHMAT");
 		exit(1);
 	}
 
 	// TODO: inizializzare struttura
+
+	ls->coda = 0;
+	ls->testa = 0;
+	ls->dim = 0;
+	init_monitor(&(ls->m), 2);
 	
 	
 	pid_t pid;
@@ -36,27 +41,41 @@ int main() {
 	for(j=0; j < N_STAMPA ; j++) {
 		
 		// TODO
-		
+		pid = fork();
+		if (pid == 0) {
 			printf("[%d]Inizio Stampa\n",getpid());
-						
+			stampa(ls);
+			exit(0);
 		}
+						
 	}
-
-	
 	for(j=0; j < N_FILTRI ; j++) {
-		
-		// TODO
-			}
+		pid = fork();	
+		if (pid == 0) {
+			filtro(ls, id_queue);
+			exit(0);
+		}	
+	}
 
 	for(j=0; j < N_GENERATORI ; j++) {
 	
 		// TODO
-			}
+		pid = fork();
+		if (pid == 0) {
+			generazione(id_queue);
+			exit(0);
+		}
+	}
 
 	// TODO: attendere terminazione processi figli
+	for(int i = 0; i < N_GENERATORI + N_STAMPA + N_FILTRI; i++){
+		wait(NULL);
+	}
 	
 	// TODO: deallocazione strutture
-	
+	remove_monitor(&(ls->m));
+	msgctl(id_queue, IPC_RMID, NULL);
+	shmctl(id_shm, IPC_RMID, NULL);
 }
 
 
